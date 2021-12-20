c8a18aaf955ba6d2a42a3d63733743339d611444
jameshannafin <jhannafin96@gmail.com>
2021-12-20 07:25:16 -0800

    Added the Game File

---
+++ challenges_basic.py
@@ -0,0 +1,45 @@
+creator = "CJamesC"
+description = """
+    I am a software Developer.
+    I am still learning.
+    I want to be a /"master/" developer
+"""
+cut = slice(11,20)
+print (description[cut])
+
+print(len(description))
+
+print (creator.strip('C'))
+
+print (creator.upper())
+
+print (description + " - " + creator.strip('C'))
+
+print("Math Examples")
+num =5
+print(num + 3)
+num += 5
+if (num == 6):
+print(num)
+else
+print('meh');
+if num > 10
+print("THe num is greater than 10")
+
+print (num is 5)
+
+y = (1:'a',2:'b')
+print(1 in y)
+
+npcs = ['skeleton','gargoyle','vampire'];
+npcs.append('hampster')
+npcs.copy(npcs_copy)
+npcs + npcs_copy
+npcs.reverse()
+
+tup_npcs{'zombie','rat','beast'}
+tup_npcs.count()
+
+
+
+
