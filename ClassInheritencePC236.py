b5c2b2983e1b648f254df3be29c4525690b85fb1
jameshannafin <jhannafin96@gmail.com>
2021-12-21 10:26:03 -0800

    Update the class inheritence file for project assignment PC265

--- ClassInheritencePC236.py
+++ ClassInheritencePC236.py
@@ -1,14 +1,28 @@
 class npc: #Non player characters
     hp = 100 #Health
     ap = 0 #Armour
-    wpn = "none #Weapon
+    wpn = "none" #Weapon
     team = "neutral" #Allegiance
 
 
     
 #////////NEUTRALS//////////////
-class farmer(npc):
-    easilyFrightened = True
+class farmer(npc):#Child Object
+    easilyFrightened = False #Is this characte easily frightened ?
+    gold = 50 #How much gold do they have?
+    specialGift = 'amulet' #<ore unique variables
+    firstname = 'Willard'
+    def inView(self): #define a function that passes information from where it was instantiated to print the complete data 
+        print("I am {}, a mere farmer, will you help me?".format(firstname) # Use {} as a wildcard to fill in with the self's rewuested variable (firstname)
+        
+
+class nobleman(npc):#Child Object
+    easilyFrightened = True #Is this characte easily frightened ?
+    gold = 250 #How much gold do they have?
+    lastname = "Pendolton"
+    specialResponse = "Ah you prove yourself worthy of a heroes reward, I talk praise of you to my family at the " + lastname + " Estate.:
+    def inView(self):
+        print("You there! Stop galavanting and come to the great {}'s aid!".format(lastname))
     
     
 #////////ENEMIES//////////////
