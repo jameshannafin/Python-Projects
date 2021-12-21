2a958c57419c5a07fc2f618995ec964a9ff90407
jameshannafin <jhannafin96@gmail.com>
2021-12-21 09:03:38 -0800

    Create ClassInheritencePC236.py

---
+++ ClassInheritencePC236.py
@@ -0,0 +1,29 @@
+class npc: #Non player characters
+    hp = 100 #Health
+    ap = 0 #Armour
+    wpn = "none #Weapon
+    team = "neutral" #Allegiance
+
+
+    
+#////////NEUTRALS//////////////
+class farmer(npc):
+    easilyFrightened = true
+    
+    
+#////////ENEMIES//////////////
+class enemy(npc): #PARENT
+    team = 'evil'
+
+class skeleton(enemy):
+    ap = 20
+    wpn = "bow"
+
+#////////FRIENLIES//////////////
+class friendly(npc):#PARENT
+    team = 'good'
+
+class knight(friendly):
+    ap = 50
+    wpn = sword
+    hasShield = true
