6a00bbd0f0dbd32da3310b7b6d21b7cf2e731a0b
jameshannafin <jhannafin96@gmail.com>
2022-02-09 09:27:36 -0800

    Commented Out AssignmentFileTransferandGUI

--- AssignmentFileTransferAndGUI.py
+++ AssignmentFileTransferAndGUI.py
@@ -1,65 +1,62 @@
 
-#Probally more imports than i need, but i do not understand the difference 
+#### #### IMPORTS #### ####
 import datetime
-import tkinter
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
 import shutil
 import os
 
-appBuilder = "James Hannafin"
-appBuildDate = datetime.date(2022,1,20)
+#DEVELOPER: James Hannafin
+#Build Date = 2022,1,20
 
 #Time Deltas
-delta_oneDay = datetime.timedelta(days = 1)
-delta_oneWeek = datetime.timedelta(days = 7)
+delta_oneDay = datetime.timedelta(days = 1) #Store time delta of one day in a var 
+delta_oneWeek = datetime.timedelta(days = 7) #Store time delta of one week in a var 
 
 #Dates
-date_today = datetime.datetime.today()
-date_yesterday = (date_today - delta_oneDay)
+date_today = datetime.datetime.today() #Store todays date in a var
+date_yesterday = (date_today - delta_oneDay) #Store yesterdays date in a var by subtracting the time delta of one day
 
-
-
-
-class ParentWindow(Frame):
+class ParentWindow(Frame): #Main (only) GUI Window
     
-    def __init__ (self, master):
-        Frame.__init__ (self) #***
-        self.master = master #***
+    def __init__ (self, master): #question 1 ??
+        Frame.__init__ (self) #??
+        self.master = master #??
         destination = "No Destination Selected"
         source = "No Source Selected"
-        dailyUpdates = IntVar() #***ERROR
+        dailyUpdates = IntVar #Declaring a var to use for the checkbox.
+    
         self.master.resizable(width=False,height=False) #Cannot resize the window
         self.master.geometry('{}x{}'.format(900,600)) #Window Size
         self.master.title("File Transfer") #App title
         self.master.config(bg='darkgray') #Window Background Color
-                          
+
         #Source Label
-        self.lbl_sourceLabel = Label(self.master,text="Source: ",font=("Helvetica",16),fg='black',bg='lightgray')
-        self.lbl_sourceLabel.grid(row=0, column = 0,padx=(30,0),pady=(30,0))
+        self.lbl_sourceLabel = Label(self.master,text="Source: ",font=("Helvetica",16),fg='black',bg='lightgray') #Create a label with the given parameters
+        self.lbl_sourceLabel.grid(row=0, column = 0,padx=(30,0),pady=(30,0)) #Position the label as indicated 
 
         #Destination Label
         self.lbl_destinationLabel = Label(self.master,text="Destination: ",font=("Helvetica",16),fg='black',bg='lightgray')
         self.lbl_destinationLabel.grid(row=1, column = 0,padx=(30,0),pady=(30,0))
 
-        #Selected Source
+        #Selected Source *** I want these to update when a source or destination selected **question 2
         self.lbl_source = Label(self.master,text=source,font=("Helvetica",16),fg='black',bg='lightblue')
         self.lbl_source.grid(row=0,column=3,padx=(30,0),pady=(30,0))
 
-        #Selected Destination
+        #Selected Destination *** I want these to update when a source or destination selected **question 2
         self.lbl_destination = Label(self.master,text=destination,font=("Helvetica",16),fg='black',bg='lightblue')
         self.lbl_destination.grid(row=1,column=3,padx=(30,0),pady=(30,0))
 
-        #Select Source BTN
+        #Select Source BTN ##Use the button widget to trigger a function which will open a file dialog 
         self.btn_selSource= ttk.Button(win, text="Select", command=self.select_source)
         self.btn_selSource.grid(row = 0, column = 2,padx=(0,90),pady=(30,0))
 
-        #Select Destination Button
+        #Select Destination Button ##Use the button widget to trigger a function which will open a file dialog 
         self.btn_selDest= ttk.Button(win, text="Select", command= self.select_destination)
         self.btn_selDest.grid(row = 1, column = 2,padx=(0,90),pady=(30,0))
 
-        #Transfer all files Button
+        #Transfer all files Button ## A buttun to manually transfer all files from the source to the destination
         self.btn_transferAll = Button(self.master, text = 'Transfer All', width=10, height = 2, command = self.transfer_all)
         self.btn_transferAll.grid(row=6,column=0,padx=(0,0),pady=(30,0))
 
@@ -74,46 +71,52 @@ class ParentWindow(Frame):
     
 
     #### Functions ####
-        
-    def select_source():#Select a source folder.
-       source= filedialog.askdirectory()
+
+    #Select a source folder.
+    def select_source(self):
+        print("Source Changed")#Confirm this in the console
+        self.source= filedialog.askdirectory()
+        self.files = os.listdir(self.source)
+
+    #Select a destination folder.
+    def select_destination(self):
+        print("Destination Changed")#Confirm this in the console
+        self.destination= filedialog.askdirectory()
        
-    def select_destination():#Select a destination folder.
-       destination= filedialog.askdirectory()
-       
-    def transfer_all():#Transfer all files from source to destination
-        files = os.listdir(master.source)
+    #Transfer all files from the source to the the destination
+    def transfer_all(self):#Transfer all files from source to destination
+        files = os.listdir(self.source)
         for i in files:
-            shutil.move(source+i,destination)
+            shutil.move(self.source+i,self.destination)
             print("A file has been transferred")
-         
-    def close(self):#Close the app
+
+    #Close the app
+    #   
+    def close(self):
         self.master.destroy()
 
 #### #### Transfer Files that are older than 24 hours  #### ####        
     
-##    if dailyUpdates == 1:
-##        print("Running the datetime autotransfer module")
-##        Run filetransfer algorithm
-##        for i in files:
-##            #We are saying move the files representred by i to their new destinations
-##            currentFile = source+i
-##            currentFileModDateTime =  os.path.getmtime(currentFile)
-##            print(currentFileModDateTime)
-##            print(date_yesterday)
-##            if currentFileModDateTime > date_yesterday:
-##                shutil.move(source+i,destination)
-##                print("A file has been transferred")
-##
+        if self.dailyUpdates == 1: #If the daily backup checkbox is marked..
+            print("Running the datetime autotransfer module")#Debug
+            for i in self.files: #Iterate through each item with a for loop
+                currentFile = self.source+i #Identify the current file 
+                currentFileModDateTime =  os.path.getmtime(currentFile) #Ascertain the time the current file was last modified
+                print("File last modified:" + currentFileModDateTime) #Print that time to the console 
+                print("Yesterday:" + date_yesterday) #Print yesterday to the console 
+                if currentFileModDateTime > date_yesterday: #*** Question 3 If last time the file was modified before yesterday, move it to the destination
+                    shutil.move(currentFile,self.destination)#Use the shutil module to .move a file to a new folder 
+                    print("A file has been transferred")
 
 
-print("Source:" + source)
-print("Daily Updates:" + str(dailyUpdates))
-print("Destination:" + destination)
-print("Todays Date:"+ str(date_today))
+        #Debugging
+        print("Source:" + self.source)
+        print("Daily Updates:" + str(self.dailyUpdates))
+        print("Destination:" + self.destination)
+        print("Todays Date:"+ str(date_today))
 
-
+## Main Loop
 if __name__ == "__main__":                  
     win = Tk()#Create initial window
-    App = ParentWindow(win)#***
+    App = ParentWindow(win)#*** Question 4
     win.mainloop()#Loop the program
--- AssignmentNewFileMaker.py
+++ AssignmentNewFileMaker.py
@@ -1,5 +1,10 @@
+#### #### #### ####
+    # FILE MAKER 
+#### #### #### ####
+    # By James Hannafin
+#### #### #### ####
 
-
+#### #### IMPORTS #### ####
 import datetime
 import tkinter
 from tkinter import *
@@ -9,40 +14,45 @@ import shutil
 import os
 from webbrowser import open_new_tab
 
-appBuilder = "James Hannafin"
-appBuildDate = datetime.date(2022,1,20)
+appStartDate = datetime.date(2022,1,20)
 
-
+#Main Parent Frame Window
 class ParentWindow(Frame):
     def __init__ (self, master):
         Frame.__init__ (self)
         self.master = master
-        selectedFile = "Select a file to modify one"
-        self.master.resizable(width=False,height=False)
-        self.master.geometry('{}x{}'.format(900,700))
-        self.master.title("File Transfer")
-        self.master.config(bg='darkgray')
+        selectedFile = "Select a file or create one to modify it" #Var for where the selected file to modify is stored 
+        self.master.resizable(width=False,height=False)#Window nonchangeable
+        self.master.geometry('{}x{}'.format(1200,700))#Window size
+        self.master.title("File Transfer") #App Title
+        self.master.config(bg='darkgray')#BG Color
                           
-        #Title
+        #Title Label
         self.lbl_titleLabel = Label(self.master,text="File Maker ",font=("Helvetica",16),fg='black',bg='lightgray')
         self.lbl_titleLabel.grid(row=0, column = 0,columnspan = 2,padx=(30,0),pady=(30,0))
-        #Instructions
-        self.lbl_title = Label(self.master,text="Create a new file or a modify an old one")
-        self.lbl_title.grid(row=1, column = 0,columnspan = 3,padx=(30,0),pady=(30,0))                  
 
+        #Instructions Label
+        self.lbl_title = Label(self.master,text="Create a new file and modify it select another basic HTML file to modify.")
+        self.lbl_title.grid(row=1, column = 0,columnspan = 3,padx=(0,0),pady=(0,0))
+
+        #Selected File Label ***Doesnt Change
+        self.lbl_selectedFile = Label(self.master,text=selectedFile)
+        self.lbl_selectedFile.grid(row=1, column = 4,padx=(30,0),pady=(30,0))
 
         #Create File BTN
         self.btn_createFile= ttk.Button(win, text="Create File", command=self.create_file)
         self.btn_createFile.grid(row = 2, column = 1,padx=(0,90),pady=(30,0))
 
+        #Modify File Text Input Box
         self.ipt_newBody = Text(master)
         self.ipt_newBody.grid(row = 3, column = 0, rowspan = 2, columnspan = 3)
-        
+            
         #Select File BTN
         self.btn_selFile= ttk.Button(win, text="Select File", command=self.select_file)
         self.btn_selFile.grid(row = 5, column = 1,padx=(0,90),pady=(30,0))
 
         #Modify File Button
+        #if selectedFile != "Select a file to modify one":
         self.btn_selDest= ttk.Button(win, text="Modify Selected", command= self.modify_file)
         self.btn_selDest.grid(row = 5, column = 2,padx=(0,90),pady=(30,0))
 
@@ -50,12 +60,10 @@ class ParentWindow(Frame):
         self.btn_close = Button(self.master, text = 'Close', width=10, height = 2,command = self.close)
         self.btn_close.grid(row=5,column=3,padx=(0,90),pady=(30,0))
 
-
-#### This function to create a new file works when ran in an isolated environment, but here it gives an error saying that I am giving an argument for its not existent parameters...
-
     #### Functions ####
-    def create_file(self):#WORKING. this function creates a new file.#
+    def create_file(self):# this function creates a new file.#
         newPage = open("newpage.html", "w")#Link a variable with a newly created file with the specified name, if it does not exist already..
+        self.selectedFile = "newpage.html"#SelectedFile var is equal to the new page
         ##The following will write a basic HTML template into the new file
         newPage.write("""
         <!DOCTYPE html>
@@ -65,12 +73,14 @@ class ParentWindow(Frame):
                         <meta charset = "utf-8">
         </head>  
         <body> 
+                
                                 <p>Stay tuned for more details!</p>
+                
         </body>  
         </html>
         """)
         newPage.close()#Close the file we opened/created because were not changing it right now 
-        open_new_tab(newPage)  #***
+        open_new_tab("newpage.html")  #Open the new page 
 
     def select_file(self):#Select a file to modify, not working.
         print("select file")
@@ -78,10 +88,26 @@ class ParentWindow(Frame):
        
     def modify_file(self):#Add content to an existing file, not working
         print("Modify file")
-        def retrieve_input():
+        def retrieve_input(self):
             newBodyText = self.ipt_newBody.get("1.0",END)
-        #Now insert this var into a specic point do I use SQLite?
-         
+            newPage = open("newpage.html", "a")#Append the file created before to get rid of "Stat tuned for more details and add body."
+            newPage.write("""
+            <!DOCTYPE html>
+            <html lang ='en'>  
+            <head>     
+                            <title>Stay Tuned</title>
+                            <meta charset = "utf-8">
+            </head>  
+            <body> 
+                    <p>"""
+                    + newBodyText + 
+                    """</p>
+            </body>  
+            </html>
+            """)
+            newPage.close()#Close the file we opened/created because were not changing it right now 
+        retrieve_input(self)#Run the retrieveInput function
+            
     def close(self):#Close the program
         self.master.destroy()
         
@@ -90,4 +116,3 @@ if __name__ == "__main__":  #Loop the program
     win = Tk()
     App = ParentWindow(win)
     win.mainloop()
-
